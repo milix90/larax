4b2dd6a7a8a3eafa3eb45a253c469ac6d5b34675
Zero One <myzerone2@gmail.com>
2021-04-17 17:44:03 +0100

    unit test mySql err. Not Works.

---
+++ tests/Unit/EmployeeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use PHPUnit\Framework\TestCase;
+
+class EmployeeTest extends TestCase
+{
+    public function test_create_new_user()
+    {
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => '123123123'
+        ]);
+
+        $this->assertTrue(!!$user);
+    }
+}
---
+++ tests/Unit/TaskTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    public function test_create_task()
+    {
+        $task = new Task();
+        $task->create([
+            'title' => 'unit test',
+            'description' => 'unit test',
+            'user_id' => 1,
+            'assign_id' => 1
+        ]);
+
+        $this->assertTrue(!!$task);
+    }
+}
